d1f00c5c2f858d5b72751355bcb0fb6e0111d9b7
kevin <kevinguevararacarrasco120@gmail.com>
2021-10-12 20:33:05 +0200

    Modificacion del archivo

--- modulos/funciones_matematicas.py
+++ modulos/funciones_matematicas.py
@@ -1,8 +1,5 @@
-#funciones matematicas que nos ayudaran a comprer los modulos en python 
-
-
 def suma(num1,num2):
-    print(num1,num2,"{i} + {j} = ",num1+num2)
+    print("La suma es: ",num1 + num2)
     
 def resta(num1,num2):
     print("La resta es: ",num1 - num2)
