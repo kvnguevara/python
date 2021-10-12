efbea063f4e588e9baa3a3944cadc5d2d49d772a
kevin <kevinguevararacarrasco120@gmail.com>
2021-10-12 22:49:15 +0200

    Paquetes de python y organizando todo

---
+++ calculo/calculos_generales.py
@@ -0,0 +1,17 @@
+def suma(num1,num2):
+    print("La suma es: ",num1 + num2)
+    
+def resta(num1,num2):
+    print("La resta es: ",num1 - num2)
+    
+def multiplicacion(num1,num2):
+    print("La multiplicacion es: ",num1*num2)
+
+def potencia(base,exponente):
+    print("El resultado es: ",base**exponente)
+
+def division(num1,num2):
+    print("La division es: ",num1/num2)
+    
+def redondear(num1):
+    print("El redondear es: ",round(num1))
---
+++ prueba_paquetes/uso_paquetes.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append(r'C:\\Users\kevin\\Desktop\\Proyect\\python')
+from calculo.calculos_generales import suma,division
+division(8,2)
+suma(8,2)  
+
+
