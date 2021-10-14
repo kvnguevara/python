f71a6e5f716ba55f309ff7feaef98208afab988e
= <=>
2021-10-14 10:59:59 +0200

    modificacion

--- convertidorCSV/convertCSV.py
+++ convertidorCSV/convertCSV.py
@@ -7,26 +7,27 @@ locExcel= (r"C:\\Users\\kguevara\\Downloads\\Libro3.xlsx")
 localCSV = (r"C:\Users\kguevara\\Desktop\PruebaCSV\csv\libroPruebas3.csv")
 
 dateFormatters ="%d/%m/%Y"
+#formateador = str.format()
 datos = pd.read_excel (locExcel, index_col=0)
 df = pd.DataFrame(datos)
 
+#fechaProximaEntrevista= obj
+#fecha_incorporacion =datatime64
+
 #Para cambiar de tipo las columnas para
-df.astype({'fechaProximaEntrevista':'str','fecha_incorporacion':'str'})
-#df.fechaProximaEntrevista = df.fechaProximaEntrevista.strptime('%d/%m/%Y')
-#df.fecha_incorporacion = pd.to_datetime(df["fechaProximaEntrevista"].astype(str), '%x')
+df['fechaProximaEntrevista']=pd.to_datetime(df['fechaProximaEntrevista'])
+df['fecha_incorporacion']=pd.to_datetime(df['fecha_incorporacion'])
+
+
+
 
 #df.fechaProximaEntrevista = pd.to_datetime(df.fechaProximaEntrevista).strftime(df.fechaProximaEntrevista, '%d/%m/%Y')
-df.fecha_incorporacion =  datetime.strptime(str(df['fecha_incorporacion']), '%d/%m/%Y')
+#df.fecha_incorporacion =  datetime.strptime(df['fecha_incorporacion'],'%d/%m/%Y')
 print(df['fechaProximaEntrevista'].dtype)
 print(df['fecha_incorporacion'].dtype)
 
-#Para convertir un cadena a objeto datatime64, que es la fecha que utiliza pandas
-"""df['fechaProximaEntrevista'].strftime(df['fechaProximaEntrevista'], errors="coerce", format='%d/%m/%Y')
-df['fecha_incorporacion'].strftime(df['fecha_incorporacion'], errors="coerce", format='%d/%m/%Y') """
 
-fecha = datetime.now().strftime(format='%d/%m/%Y')
 print(df['fechaProximaEntrevista'])
 print(df['fecha_incorporacion'])
-print(fecha)
 print(df.head())
-#print(pd.to_datetime(fecha, format='%Y-%d-%m'))
+
